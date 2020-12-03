6b5dbb5765c7fe51466102cc1a44f42815ff2319
Arif <arifboyz16@gmail.com>
2020-12-03 09:03:11 +0700

    fixing logic active month

--- service-python/dashboardUser/app.py
+++ service-python/dashboardUser/app.py
@@ -60,27 +60,36 @@ def dashboardAttendanceUser():
         data = {}
         if user != 401:
             today = dt.datetime.today().date()
+            start = dt.datetime(year=today.year, month=today.month, day=1).date()
             if month: 
-                today = dt.datetime(year=today.year, month=int(month), day=1).date()
-            start = today
-            end = last_day_of_month(today)+timedelta(days=1)
+                start = dt.datetime(year=today.year, month=int(month), day=1).date()
+            end = last_day_of_month(start)+timedelta(days=1)
             dateRange = np.arange(np.datetime64(start), np.datetime64(end), dtype='datetime64[D]')
-            # dateRangeFromNow = np.arange(np.datetime64(start), np.datetime64(today+timedelta(days=1)), dtype='datetime64[D]')
-            # listBusdayFromNow = np.busdaycalendar(holidays=dateRangeFromNow, weekmask=busmask_names)
+            dateRangeFromNow = np.arange(np.datetime64(start), np.datetime64(today+timedelta(days=1)), dtype='datetime64[D]')
+            listBusdayFromNow = np.busdaycalendar(holidays=dateRangeFromNow, weekmask=busmask_names)
             listBusday = np.busdaycalendar(holidays=dateRange, weekmask=busmask_names)
             listWeekend = np.busdaycalendar(holidays=dateRange, weekmask=weekmask_names)
+
             presence = 0
             noPresence = 0
-            for i in listBusday.holidays:
-                if getPresence(mongoClient, user['user_id'], str(i)):
-                    presence += 1
-                else:
-                    noPresence += 1
+            if month == None or today.month == int(month):
+                for i in listBusdayFromNow.holidays:
+                    if getPresence(mongoClient, user['user_id'], str(i)):
+                        presence += 1
+                    else:
+                        noPresence += 1
+            else:
+                for i in listBusday.holidays:
+                    if getPresence(mongoClient, user['user_id'], str(i)):
+                        presence += 1
+                    else:
+                        noPresence += 1
+
             presenceWeekend = 0
             for i in listWeekend.holidays:
                 if getPresence(mongoClient, user['user_id'], str(i)):
                     presenceWeekend += 1
-            # busDaysFromNow = len(listBusdayFromNow.holidays)
+            busDaysFromNow = len(listBusdayFromNow.holidays)
             busDays = len(listBusday.holidays)
             weekEnd = len(listWeekend.holidays)
 
@@ -124,10 +133,10 @@ def dashboardReportUser():
         data = {}
         if user != 401:
             today = dt.datetime.today().date()
+            start = dt.datetime(year=today.year, month=today.month, day=1).date()
             if month: 
-                today = dt.datetime(year=today.year, month=int(month), day=1).date()
-            start = today
-            end = last_day_of_month(today)+timedelta(days=1)
+                start = dt.datetime(year=today.year, month=int(month), day=1).date()
+            end = last_day_of_month(start)+timedelta(days=1)
 
             totalOfficeHourUserYear = countOfficeHourUserYear(mongoClient, user['user_id'], today.year)
             totalOfficeHourUserMonthly = countOfficeHourUserMonthly(mongoClient, user['user_id'], str(start), str(end))
