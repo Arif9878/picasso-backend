692f709c8db4095713cfca66bba118546e831e46
Arif Wicaksono <arif@Arifs-MacBook-Pro.local>
2021-04-03 00:36:51 +0700

    improve report user

--- service-python/monthlyreport/app.py
+++ service-python/monthlyreport/app.py
@@ -1,4 +1,4 @@
-import os, json, sentry_sdk
+import os, numpy as np, json, sentry_sdk, datetime
 
 from os.path import join, dirname, exists
 from dotenv import load_dotenv
@@ -8,7 +8,8 @@ from pymongo import MongoClient
 from flask_opentracing import FlaskTracing
 from sentry_sdk.integrations.flask import FlaskIntegration
 
-from utils import getCountHours, getCountLogbook, convertFunc, queryAccount, config_jaeger
+from holiday_query import getListHoliday
+from utils import getCountHours, getCountLogbook, getListDateLogbook, convertFunc, queryAccount, config_jaeger
 
 dotenv_path = ''
 if exists(join(dirname(__file__), '../../.env')):
@@ -57,6 +58,8 @@ db = SQLAlchemy(app)
 jaeger_tracer = config_jaeger(jaeger_host, jaeger_port).initialize_tracer()
 tracing = FlaskTracing(jaeger_tracer)
 
+busmask_names = 'Mon Tue Wed Thu Fri'
+
 @app.route('/api/monthly-report/')
 @tracing.trace('path', 'method', 'META', 'path_info', 'content_type')
 def listUserByUnit():
@@ -69,13 +72,33 @@ def listUserByUnit():
         query = queryAccount(search='%'+search+'%', divisi=divisi)
     result = db.session.execute(query)
     response = []
+
+    if start_date and end_date:
+        start_date = datetime.datetime.strptime(start_date+'-0:0:0', '%Y-%m-%d-%H:%M:%S')
+        end_date = datetime.datetime.strptime(end_date+'-23:59:59', '%Y-%m-%d-%H:%M:%S')
+
+        dateRange = np.arange(np.datetime64(start_date), np.datetime64(end_date), dtype='datetime64[D]')
+        listBusday = np.busdaycalendar(holidays=dateRange, weekmask=busmask_names)
+        listHoliday = getListHoliday(mongoClient, np, start_date.year, end_date.month)
+
+        # Delete working days if there are holidays
+        listBusday = np.array(list(filter(lambda x: x not in listHoliday, listBusday.holidays)))
+
     if result.returns_rows == False:
         return response
     else:
         for i in result:
+            listDayNoLogbook = []
+            dataFillingLogbook = 0
+            if start_date and end_date:
+                listDateLogbook = getListDateLogbook(mongoClient, i.id, np, start_date, end_date)
+                listDayNoLogbook = np.array(list(filter(lambda x: x not in listDateLogbook, listBusday)))
+                dataFillingLogbook = round((len(listDateLogbook)/len(listBusday))*100, 2)
+                listDayNoLogbook = list(np.datetime_as_string(listDayNoLogbook, unit='D'))
+            
             totalReport = getCountLogbook(mongoClient, i.id, start_date, end_date)
             totalHours = getCountHours(mongoClient, i.id, start_date, end_date)
-            response.append(convertFunc(i, totalReport, totalHours))
+            response.append(convertFunc(i, totalReport, totalHours, dataFillingLogbook, listDayNoLogbook))
     return json.dumps(response)
 
 port = os.environ.get('MONTHLY_REPORT_PORT', 80)
